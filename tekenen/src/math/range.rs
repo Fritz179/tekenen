edd4a8bf907ec91da3b00452db2b145c4e9c4c39
Fritz 179 <fabrizio.cortesi179@gmail.com>
2024-03-21 16:36:32 +0100

    Added Range Trait

--- tekenen/src/math/range.rs
+++ tekenen/src/math/range.rs
@@ -2,15 +2,31 @@ use std::{iter::Sum, ops::{Add, AddAssign}};
 
 use super::Vec2;
 
-/// Garanteed to have a valid range (min <= max)
-#[derive(Debug, Default, Clone)]
-pub struct Range<T: std::cmp::PartialOrd + Copy = i32> {
-    pub min: T,
-    pub max: T,
+// TODO: Add Add, Sub
+pub trait RangeTrait<OuterT, InnerT> 
+{
+    fn new(min: InnerT, max: InnerT) -> Self;
+    fn new_definite(value: InnerT) -> Self;
+    fn new_min_priority(min: OuterT, max: OuterT) -> Self;
+    fn new_max_priority(min: OuterT, max: OuterT) -> Self;
+    fn get_min(&self) -> OuterT;
+    fn get_max(&self) -> OuterT;
+
+    fn constrain(&self, value: InnerT) -> InnerT;
+
+    /// Get Some(T) if both min and max are the same
+    fn identify(&self) -> Option<InnerT>;
 }
 
-impl Range {
-    pub fn new(min: i32, max: i32) -> Self {
+/// Garanteed to have a valid range (min <= max)
+#[derive(Debug, Default, Clone)]
+pub struct Range<T: PartialOrd + Copy = i32> {
+    min: T,
+    max: T,
+}
+
+impl<T: PartialOrd + Copy> RangeTrait<T, T> for Range<T> {
+    fn new(min: T, max: T) -> Self {
         assert!(min <= max);
 
         Self {
@@ -19,14 +35,14 @@ impl Range {
         }
     }
 
-    pub fn new_definite(value: i32) -> Self {
+    fn new_definite(value: T) -> Self {
         Self {
             min: value,
             max: value
         }
     }
 
-    pub fn new_min_priority(min: i32, max: i32) -> Self {
+    fn new_min_priority(min: T, max: T) -> Self {
         if max < min {
             Self {
                 min,
@@ -40,7 +56,29 @@ impl Range {
         }
     }
 
-    pub fn constrain(&self, value: i32) -> i32 {
+    fn new_max_priority(min: T, max: T) -> Self {
+        if max < min {
+            Self {
+                min: max,
+                max
+            }
+        } else {
+            Self {
+                min,
+                max
+            }
+        }
+    }
+
+    fn get_min(&self) -> T {
+        self.min
+    }
+
+    fn get_max(&self) -> T {
+        self.max
+    }
+
+    fn constrain(&self, value: T) -> T {
         if value < self.min {
             self.min
         } else if value > self.max {
@@ -49,9 +87,17 @@ impl Range {
             value
         }
     }
+
+    fn identify(&self) -> Option<T> {
+        if self.min == self.max {
+            Some(self.min)
+        } else {
+            None
+        }
+    }
 }
 
-impl<T: std::cmp::PartialOrd + Copy + Add<Output = T>> Add<T> for Range<T> {
+impl<T: PartialOrd + Copy + Add<Output = T>> Add<T> for Range<T> {
     type Output = Self;
 
     fn add(self, value: T) -> Self {
@@ -62,7 +108,7 @@ impl<T: std::cmp::PartialOrd + Copy + Add<Output = T>> Add<T> for Range<T> {
     }
 }
 
-impl<T: std::cmp::PartialOrd + Copy + Add<Output = T>> Add for Range<T> {
+impl<T: PartialOrd + Copy + Add<Output = T>> Add for Range<T> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -73,7 +119,7 @@ impl<T: std::cmp::PartialOrd + Copy + Add<Output = T>> Add for Range<T> {
     }
 }
 
-impl<T: std::cmp::PartialOrd + Copy + Default + Add<Output = T>> Sum for Range<T> {
+impl<T: PartialOrd + Copy + Default + Add<Output = T>> Sum for Range<T> {
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
         iter.fold(Self::default(), |acc, x| acc + x)
     }
@@ -89,29 +135,19 @@ impl Range<i32> {
     }
 }
 
+impl<T: PartialOrd + Copy> From<Range<T>> for IndefRange<T> {
+    fn from(range: Range<T>) -> Self {
+        Self::new(range.get_min(), range.get_max())
+    }
+}
+
 #[derive(Debug, Default, Clone)]
-pub struct IndefRange<T: std::cmp::PartialOrd + Copy = i32> {
+pub struct IndefRange<T: PartialOrd + Copy = i32> {
     min: Option<T>,
     max: Option<T>,
 }
 
-impl<T: std::cmp::PartialOrd + Copy> IndefRange<T> {
-    pub fn new(min: T, max: T) -> Self {
-        assert!(min <= max);
-
-        Self {
-            min: Some(min), 
-            max: Some(max)
-        }
-    }
-
-    pub fn new_definite(value: T) -> Self {
-        Self {
-            min: Some(value),
-            max: Some(value)
-        }
-    }
-
+impl<T: PartialOrd + Copy> IndefRange<T> {
     pub fn new_option(min: Option<T>, max: Option<T>) -> Self {
         if let Some(min) = min {
             if let Some(max) = max {
@@ -120,12 +156,34 @@ impl<T: std::cmp::PartialOrd + Copy> IndefRange<T> {
         }
 
         Self {
-            min,
+            min, 
             max
         }
     }
 
-    pub fn new_min_priority(min: Option<T>, max: Option<T>) -> Self {
+    pub fn is_constrained(&self) -> bool {
+        self.min.is_some() && self.max.is_some()
+    }
+}
+
+impl<T: PartialOrd + Copy> RangeTrait<Option<T>, T> for IndefRange<T> {
+    fn new(min: T, max: T) -> Self {
+        assert!(min <= max);
+
+        Self {
+            min: Some(min), 
+            max: Some(max)
+        }
+    }
+
+    fn new_definite(value: T) -> Self {
+        Self {
+            min: Some(value),
+            max: Some(value)
+        }
+    }
+
+    fn new_min_priority(min: Option<T>, max: Option<T>) -> Self {
         if let Some(min) = min {
             if let Some(max) = max {
                 if max < min {
@@ -143,7 +201,7 @@ impl<T: std::cmp::PartialOrd + Copy> IndefRange<T> {
         }
     }
 
-    pub fn new_max_priority(min: Option<T>, max: Option<T>) -> Self {
+    fn new_max_priority(min: Option<T>, max: Option<T>) -> Self {
         if let Some(min) = min {
             if let Some(max) = max {
                 if max < min {
@@ -161,15 +219,15 @@ impl<T: std::cmp::PartialOrd + Copy> IndefRange<T> {
         }
     }
 
-    pub fn get_min(&self) -> Option<T> {
+    fn get_min(&self) -> Option<T> {
         self.min
     }
 
-    pub fn get_max(&self) -> Option<T> {
+    fn get_max(&self) -> Option<T> {
         self.max
     }
 
-    pub fn constrain(&self, value: T) -> T {
+    fn constrain(&self, value: T) -> T {
         if let Some(min) = self.min {
             if value < min {
                 return min
@@ -185,15 +243,7 @@ impl<T: std::cmp::PartialOrd + Copy> IndefRange<T> {
         value
     }
 
-    pub fn is_constrained(&self) -> bool {
-        self.min.is_some() || self.max.is_some()
-    }
-
-    pub fn is_constrained_both(&self) -> bool {
-        self.min.is_some() && self.max.is_some()
-    }
-
-    pub fn identify(&self) -> Option<T> {
+    fn identify(&self) -> Option<T> {
         let min = self.min?;
         let max = self.max?;
 
@@ -203,6 +253,9 @@ impl<T: std::cmp::PartialOrd + Copy> IndefRange<T> {
             None
         }
     }
+}
+
+impl<T: PartialOrd + Copy> IndefRange<T> {
 
     /// Result fits in both
     /// Result is stricter or equeal
@@ -286,7 +339,7 @@ impl IndefRange<i32> {
 }
 
 // Add T to each component
-impl<T: std::cmp::PartialOrd + Copy + Add<Output = T>> Add<T> for IndefRange<T> {
+impl<T: PartialOrd + Copy + Add<Output = T>> Add<T> for IndefRange<T> {
     type Output = Self;
 
     fn add(self, rhs: T )-> Self {
@@ -300,7 +353,7 @@ impl<T: std::cmp::PartialOrd + Copy + Add<Output = T>> Add<T> for IndefRange<T> 
     }
 }
 
-impl<T: std::cmp::PartialOrd + Copy + Add<Output = T>> Add for IndefRange<T> {
+impl<T: PartialOrd + Copy + Add<Output = T>> Add for IndefRange<T> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
